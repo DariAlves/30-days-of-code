c9911f2993810534a351010d06498c22f6315e10
Dari Alves <darieltondf@gmail.com>
2020-06-08 19:54:34 -0300

    day 05

---
+++ day_05/loops.js
@@ -0,0 +1,15 @@
+/*
+    Task
+
+    * Given an integer, n, print its first 10 multiples. 
+    Each multiple n x i (where 1 <= i <= 10) should be printed on a new line in the form: n x i = result.
+
+*/
+
+function firstTenMultiples(n) {
+    for(let i = 1; i <= 10; i++) {
+        console.log(`${n} x ${i} = ${n * i}`);
+    }
+}
+
+
